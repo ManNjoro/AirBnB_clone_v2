2d27777f4597966a78f49bf49fa1c53ff3d70445
ManNjoro <elijohnmwoho@gmail.com>
2023-08-23 12:50:54 -0700

    added DB storage and implemented sqlAlchemy

--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if models.HBNB_TYPE_STORAGE == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state')
+    else:
+        name = ''
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.HBNB_TYPE_STORAGE != 'db':
+        @property
+        def cities(self):
+            from models import storage
+            city_instances = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_instances.append(city)
+            return city_instances
