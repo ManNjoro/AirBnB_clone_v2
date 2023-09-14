93ba5ad5cb7bc1cebf3afe8e69c7298aeda5d775
ManNjoro <elijohnmwoho@gmail.com>
2023-09-14 09:29:35 -0700

    corrections

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,41 +1,32 @@
 #!/usr/bin/python3
 """
-Fabric script to distribute an archive to your web servers using do_deploy
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
 
-from fabric.api import env, put, run, local
+from fabric.api import put, run, env
 from os.path import exists
 from datetime import datetime
-
 env.hosts = ['54.85.129.38', '18.206.208.173']
-env.user = 'ubuntu'
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """deploys to remote server"""
-    if not exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
+    file_name = archive_path.split("/")[-1]
+    no_ext = file_name.split(".")[0]
+    path = "/data/web_static/releases/"
     try:
         put(archive_path, '/tmp/')
-        archive_filename = archive_path.split('/')[-1]
-        release_folder = '/data/web_static/releases/{}'.format(
-            archive_filename.split('.')[0])
-        run('mkdir -p {}'.format(release_folder))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_folder))
-
-        run('rm /tmp/{}'.format(archive_filename))
-        run('mv {}/web_static/* {}'.format(release_folder, release_folder))
-        run('rm -rf {}/web_static'.format(release_folder))
-        current_link = '/data/web_static/current'
-        if exists(current_link):
-            run('rm {}'.format(current_link))
-        run('ln -s {} {}'.format(release_folder, current_link))
-
-        symlink_path = run('readlink -f {}'.format(current_link))
-        if symlink_path == release_folder:
-            return True
-        else:
-            return False
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
     except Exception as e:
         return False
